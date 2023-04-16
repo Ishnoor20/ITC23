cd13d57e7cca72017c2538564573428a58eea872
Ishnoor20 <130926050+Ishnoor20@users.noreply.github.com>
2023-04-17 00:02:45 +0530

    Create Q4str.py

---
+++ Q4str.py
@@ -0,0 +1,9 @@
+#a,b,c,d are the required values and their type is specified 
+a=25
+b= int('25')
+c= float(25.0)
+d = str(a+b+c)
+#type is mentioned as string as the output is required to be a string value 
+print(str(a+b+c))
+
+print(type(d))
