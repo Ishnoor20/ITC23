e9d2a0e0a033a4d66278b6c7065f1f22617477c1
Ishnoor20 <130926050+Ishnoor20@users.noreply.github.com>
2023-04-16 23:55:14 +0530

    Create Q1avg.py

---
+++ Q1avg.py
@@ -0,0 +1,10 @@
+# Take input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+# Calculate average
+average = (num1 + num2 + num3) / 3
+
+# Display the result
+print("The average of", num1, ",", num2, "and", num3, "is", average)
